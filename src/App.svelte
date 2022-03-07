8deff2890ae660c355718c556d79ce1d7fee9ddf
Borys Medvediev <borys.medvediev@auto1.com>
2022-03-07 23:25:47 +0200

    fix bg issue

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,6 @@
   import Footer from "./components/Footer.svelte";
 </script>
 
-<img class="bg-pattern" src="images/bg-pattern.svg" alt="bg-pattern" />
-
 <Header
   title="Simple, traffic-based pricing"
   subTitle="Sign-up for our 30-day trial. No credit card required."
@@ -21,14 +19,6 @@
   main {
     flex: 1 0 auto;
     padding: 0 24px;
-  }
-
-  .bg-pattern {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: -1;
   }
 
   @media (min-width: 700px) {
